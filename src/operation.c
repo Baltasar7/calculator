982c617a983d3318390a17b91cc7456cd8e4e0ca
Guest Guest <guest@example.com>
2022-05-06 09:04:05 +0900

    Change operate function type int to int32_t.

--- src/operation.c
+++ src/operation.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int operate_add(int left, int right) {
+int32_t operate_add(int32_t left, int32_t right) {
     return left + right;
 }
 
-int operate_sub(int left, int right) {
+int32_t operate_sub(int32_t left, int32_t right) {
     return left - right;
 }
 
-int operate_multi(int left, int right) {
+int32_t operate_multi(int32_t left, int32_t right) {
     return left * right;
 }
 
-int operate_div(int left, int right) {
+int32_t operate_div(int32_t left, int32_t right) {
     if(right == 0) {
         printf("Error: Division by zero.\n");
         exit(-1);
@@ -21,26 +21,26 @@ int operate_div(int left, int right) {
     return left / right;
 }
 
-int operate_mod(int left, int right) {
+int32_t operate_mod(int32_t left, int32_t right) {
     return left % right;
 }
 
-int operate_bit_lshift(int left, int right) {
+int32_t operate_bit_lshift(int32_t left, int32_t right) {
     return left << right;
 }
 
-int operate_bit_rshift(int left, int right) {
+int32_t operate_bit_rshift(int32_t left, int32_t right) {
     return left >> right;
 }
 
-int operate_bit_and(int left, int right) {
+int32_t operate_bit_and(int32_t left, int32_t right) {
     return left & right;
 }
 
-int operate_bit_or(int left, int right) {
+int32_t operate_bit_or(int32_t left, int32_t right) {
     return left | right;
 }
 
-int operate_bit_invert(int left, int right) {
+int32_t operate_bit_invert(int32_t left, int32_t right) {
     return left ^ right;
 }
